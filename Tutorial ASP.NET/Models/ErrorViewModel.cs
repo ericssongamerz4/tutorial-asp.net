5dc8cea377fd85faaf67c6d066cb9442e01941aa
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-01-21 10:51:51 -0600

    Agregar archivos de proyecto.

---
+++ Tutorial ASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tutorial_ASP.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
